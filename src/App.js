482b560c32578680abe943e5626bea3d9dab4689
Sachinsubedi293 <sachinsubedi616@gmail.com>
2023-03-13 12:39:58 +0545

    Test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="Main">
+      <div className="Logo"></div>
+      <div className="Text">Empower through Innovation</div>
+      <div className="Commingsoon"></div>
+      <div className="Rocket"></div>
+      <div className="Smoke"></div>
+    </div>
+  );
+}
+
+export default App;

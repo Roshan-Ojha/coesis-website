a3f97dd14359dc7ac4c33e2355bb63b7d809c675
Sachinsubedi293 <sachinsubedi616@gmail.com>
2023-03-13 13:35:41 +0545

    withoutresponsive

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-
 import './App.css';
-
+import Smoke from './Assests/Vector 2.png';
+import Rocket from './Assests/Rocket.png';
+import Coming from './Assests/comingsoon.png';
+import Logo  from './Assests/Logo.png';
 function App() {
   return (
     <div className="Main">
-      <div className="Logo"></div>
-      <div className="Text">Empower through Innovation</div>
-      <div className="Commingsoon"></div>
-      <div className="Rocket"></div>
-      <div className="Smoke"></div>
+      <div className="Overlay">
+      <div className="Logo"><img src={Logo} alt="Coesis Logo" /></div>
+      <div className="Text"><center>Empower through Innovation</center></div>
+      <div className="Commingsoon"><img src={Coming} alt="Comming Soon Logo" /></div>
+      <div className="Rocket"><img src={Rocket} alt="Rocket" /></div>
+      <div className="Smoke"><img src={Smoke} alt="Smoke" />
+        <div className="OverlayText"><center>Organized by COESIS</center></div>
+      </div>
+      </div>
     </div>
   );
 }
